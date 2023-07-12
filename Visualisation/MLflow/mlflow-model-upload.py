43fe21e383e577dc6de0608d9eef34e6a8a3ff27
ambiddisco <129688970+ambiddisco@users.noreply.github.com>
2023-07-12 17:33:39 +0200

    Update mlflow-model-upload.py

--- Visualisation/MLflow/mlflow-model-upload.py
+++ Visualisation/MLflow/mlflow-model-upload.py
@@ -1,16 +1,23 @@
+# Before running this code make sure you downloaded the requirements ("pip install tensorflow mlflow")
+# and run "mlflow server" in a terminal window. This will start the localhost server for visualisation 
+# and model repository upload.
+
+
+# Imports for model creation:
 import tensorflow as tf
 from tensorflow import keras
 from keras.datasets import mnist     
 from keras.models import Sequential  
-from keras.layers.core import Dense, Dropout, Activation 
+from keras.layers import Dense, Dropout, Activation 
 from keras.utils import np_utils 
-import mlflow
 
+# Imports for upload to the MLflow model registry:
+import mlflow
 from mlflow.models.signature import infer_signature
 
 # This code is for upload of a trained model to the model registry. Since we have run multiple
-# visualisations of the 6 model variations in the other scripts, here we will upload only the one
-# that has the best accuracy, which is model 1:
+# visualisations of the 6 model variations in the other scripts, here we will upload only the one with 
+# the best accuracy, which is model 1:
 
 # Separating the data into training and testing sets
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
@@ -19,7 +26,7 @@ from mlflow.models.signature import infer_signature
 X_train_divided = X_train / 255
 X_test_divided = X_test / 255
 
-# Model creation, compilation and training, using the best hyperparameters
+# Model creation, compilation and training, using the best hyperparameters:
 model = Sequential([
     keras.layers.Flatten(input_shape = (28,28)),
     keras.layers.Dense(100, 'relu'),
@@ -30,9 +37,18 @@ model.compile(
     loss = 'sparse_categorical_crossentropy',
     metrics = ['accuracy']
 )
-history = model.fit(X_train_divided, y_train, epochs = 5, verbose = 1)
-score = model.evaluate(X_test_divided, y_test)
+model.fit(X_train_divided, y_train, epochs = 5, verbose = 1)
+model.evaluate(X_test_divided, y_test)
 
-# Model upload
-signature = infer_signature(X_test_divided, model.predict(X_test_divided))
-mlflow.tensorflow.log_model(model, "MNIST_best_model", signature=signature)
+# Before uploading the model, the URL should e defined. By default, localhost:5000 is used
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+client = mlflow.tracking.MlflowClient()
+
+# Creating and naming experiment...
+experiment_name = "MNIST model upload"
+experiment_id = mlflow.create_experiment(experiment_name)
+
+with mlflow.start_run(experiment_id = experiment_id, run_name = "model-upload") as run:
+    # Model upload:
+    signature = infer_signature(X_test_divided, model.predict(X_test_divided))
+    mlflow.tensorflow.log_model(model, "MNIST_model", registered_model_name = "best_model", signature=signature)
