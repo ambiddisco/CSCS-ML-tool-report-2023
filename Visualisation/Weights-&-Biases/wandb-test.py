5174de16bd8bea8fa02f1af621f60a64b999de9e
ambiddisco <129688970+ambiddisco@users.noreply.github.com>
2023-07-12 18:16:23 +0200

    Update wandb-test.py

--- Visualisation/Weights-&-Biases/wandb-test.py
+++ Visualisation/Weights-&-Biases/wandb-test.py
@@ -1,9 +1,11 @@
+# Before starting to log metrics to W&B, you need to start its server from a terminal window.
+# Open a terminal and input "wandb server start"
+
 import tensorflow as tf
 from tensorflow import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Activation
 import wandb
 
 # The MNIST classifier dataset is a set of images, made up of 28x28 pixels, of value between 0 and 255.
@@ -20,10 +22,10 @@ first_layer_activation = ["relu", "sigmoid", "softmax", "relu", "sigmoid", "soft
 second_layer_nodes = [10, 15, 25, 25, 15, 10]
 second_layer_activation = ["sigmoid", "softmax", "relu", "relu", "sigmoid", "softmax"]
 
-# To log anything to the wandb.ai server, you need to login first. Either you can log in with "wand.login()"
-# and then navigate to wandb.ai/authorize to copy your API key, so that you can paste it when promted in the terminal,
-# or, if you already have your API key, you can login with "wandb.login(key='your key goes here')". The key here
-# is a string and must go inside quotes.
+# To log anything to the wandb.ai server, you need to start it from a terminal with "wandb server start", then 
+# you also need to login. Either you can log in with "wand.login()" and then navigate to wandb.ai/authorize to 
+# copy your API key, so that you can paste it when promted in the terminal, or, if you already have your API key, 
+# you can login with "wandb.login(key='your key goes here')". The key here is a string and must go inside quotes.
 wandb.login()
 
 for run_number in range(6):
